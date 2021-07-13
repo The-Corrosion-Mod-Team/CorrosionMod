015b91279a64eec4768de3f8bd90809bfad6625b
Hibubs <87348464+Hibubs@users.noreply.github.com>
2021-07-12 19:13:17 -0700

    Add files via upload

---
+++ start.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace hibubsmod.projectiles
+{
+	public class starts : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("starter item");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.aiStyle = 3;
+			projectile.width = 20;
+			projectile.height = 20;
+			projectile.friendly = true;
+			projectile.magic = true;
+			projectile.light = 10f;
+		}
+	}
+}
