f363e46b137bd278f93575b5a9fc0b7b9d4adc10
Hibubs <87348464+Hibubs@users.noreply.github.com>
2021-07-13 20:01:07 -0700

    Add files via upload

---
+++ nightfall.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Hibubsmod.Projectiles;
+namespace Hibubsmod.Buffs
+
+{
+	public class nightfall : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Nightfall");
+			Description.SetDefault("placeholder");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<feller>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
